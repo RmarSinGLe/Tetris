2218b2ce31b9b9690ef5d57d46e9b41070f80778
RmarSinGLe <1980347585@qq.com>
2024-09-10 09:17:35 +0800

    wallkicks logic and add art source

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -119,8 +119,8 @@ public class Piece : MonoBehaviour
                     break;
 
                 default:
-                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
-                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
                     break;
             }
 
